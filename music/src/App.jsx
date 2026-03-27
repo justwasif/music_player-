f339c8e4ef45509b15a53ce59ccb71ee64ca9b5f
wasif <wasif.iit.ansari@gmail.com>
2026-03-27 16:18:54 +0530

    feat:basic localstorage upload done

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -1,11 +1,59 @@
 import './App.css'
+import { useState,useEffect } from 'react';
 
 export default function App() {
+  let [local,setLocal]=useState()
+  const handleUpload= (e)=>{
+    // const file=e.target.files[0]
+    const sun=new FileReader();
+    //jab puri file load hojaye usske baad
+    sun.onload=()=>{
+      const convert=sun.result
+      localStorage.setItem("music",convert);
+      setLocal(convert);
+      alert("sumbitted")
+
+
+    };
+    // sun.readAsDataURL(file);
+    console.log("sumbit hogaya")
+   
+    
+  };
+  const handleDownload=()=>{
+    let download=localStorage.getItem("music");
+    console.log("dikhna to chiyea")
+    const a=document.createElement("a");
+    a.href=download;
+    a.download="chaman";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+  const handleplat=()=>{
+    const play=localStorage.getItem("music");
+    setLocal(play);
+    
+    console.log("chala??",play)
+  };
+  const clear=()=>{
+    localStorage.removeItem("music");
+    localStorage.clear()
+    console.log("done")
+
+  }
+  
+  
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold text-blue-400">
-        Tailwind 3.4.18 Works 🚀
-      </h1>
-    </div>
+   <div>
+    <h1>upload</h1>
+    <input type='file' onChange={handleUpload} /> 
+    <button onClick={handleDownload}>handleDownload</button>
+    <br/>
+    <button onClick={handleplat}>play</button>
+    {local &&<audio src={local} controls autoPlay/>}
+    <br/>
+    <button onClick={clear}>clear</button>
+   </div>
   );
 }
